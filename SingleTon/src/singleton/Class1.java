1b4a4af15ffc7836f707ab0494b47a0838c54a30
mohamedaligaber <mohamed.aly.201412@gmail.com>
2017-02-14 04:50:43 -0800

    mohamed lai v3

--- SingleTon/src/singleton/Class1.java
+++ SingleTon/src/singleton/Class1.java
@@ -29,9 +29,15 @@ public class Class1 {
         System.out.println("hello"+s);
     }
     
+    private void testNewFunction()
+    {
+        System.out.println("ay kalam");
+    }
+    
     public static void main(String[] args) {
         Class1 c = new Class1();
         c.mohamedali();
+        c.testNewFunction();
     }
     
     
