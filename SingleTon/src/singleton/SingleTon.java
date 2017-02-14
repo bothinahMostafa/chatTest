d7a5581a8f66c55645b08c749fec40ae03ffecef
mohamedaligaber <mohamed.aly.201412@gmail.com>
2017-02-14 05:21:43 -0800

    mohamed ali v10

--- SingleTon/src/singleton/SingleTon.java
+++ SingleTon/src/singleton/SingleTon.java
@@ -14,11 +14,18 @@ public class SingleTon {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         //bothinah start
-        
+            
         //bothinah end
         //mohamed start
+            
+            Class1 c5 = new Class1();
+            c5.bothinah();
+            c5.newMethod();
+            c5.sayHello();
+            c5.test5();
         
         //mohamed end
     }
