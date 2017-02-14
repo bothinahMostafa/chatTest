c7d5f69ca8bd8043691b8f028c5d3b0169d839ca
mohamedaligaber <mohamed.aly.201412@gmail.com>
2017-02-14 04:33:55 -0800

    mohamed ali v1

---
+++ SingleTon/src/singleton/Class1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package singleton;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class Class1 {
+   
+    //start of bothinah
+    
+    //end of bothinah
+    
+    //start of mohamed
+    
+    
+    private void mohamedali()
+    {
+        String s = "mohamed";
+        System.out.println("hello"+s);
+        s = "ali";
+        System.out.println("hello"+s);
+    }
+    
+    public static void main(String[] args) {
+        Class1 c = new Class1();
+        c.mohamedali();
+    }
+    
+    
+    //end of mohamed
+    
+   
+    
+}
