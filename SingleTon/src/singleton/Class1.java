5b095e48dbab867e2441b7874d29a130e18b0efa
bassant <bothinah.mostafa1@gmail.com>
2017-02-14 14:32:22 +0200

    bothinah v1

---
+++ SingleTon/src/singleton/Class1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package singleton;
+
+/**
+ *
+ * @author Pc
+ */
+public class Class1 {
+    //start of bothinah
+        public void sayHello(){
+            System.err.println("hello bothinah");
+        }
+    //end of bothinah
+    
+    //start of mohamed 
+    
+    //end of mohamed
+}
